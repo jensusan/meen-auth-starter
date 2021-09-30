35ec00102158256572ffd58ddb78349fbc582b3d
Jennifer Susan <jennifersusan@Jennifers-MBP.cable.rcn.com>
2021-09-30 13:51:47 -0500

    files created

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+// Dependencies
+const bcrypt = require('bcrypt');
+const express = require('express');
+const userRouter = express.Router();
+const User = require('../models/user.js');
+
+// New (registration page)
+
+// Create (registration route)
+userRouter.post('/', (req, res) => {
+    //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    res.send(req.body);
+});
+
+// Export User Router
+module.exports = userRouter;
